7d621426eb2b2d1a6c596fdb4019e1c1d87aeaf8
SodokinMarius <yaomariussodokin@gmail.com>
2022-09-03 14:23:04 +0100

    feat : create and add expenses urls

--- incomeexpensisesapi/urls.py
+++ incomeexpensisesapi/urls.py
@@ -23,7 +23,8 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("auth/",include('authentication.urls')),
+    path("",include('authentication.urls')),
+    path('',include('expenses.urls')),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
