288181ffb055577254f66338d8f58e081eca6119
SodokinMarius <yaomariussodokin@gmail.com>
2022-09-03 09:03:23 +0100

    feat: add login urls

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path,include 
 
-from .views import RegisterView,VerifyEmail
+from .views import RegisterView,VerifyEmail,LoginAPIView
 
 
 urlpatterns = [
     path('register/',RegisterView.as_view(),name='register'),
     path('verify_email/',VerifyEmail.as_view(),name='verify_email'),
+    path('login/',LoginAPIView.as_view(),name='login')
 
 ]
