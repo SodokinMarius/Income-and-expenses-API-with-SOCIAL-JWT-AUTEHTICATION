fdde82fe123c2d418b2bb8c49052879049350ab3
SodokinMarius <yaomariussodokin@gmail.com>
2022-09-03 12:15:52 +0100

    feat: add Expensive models and migrations

--- expenses/models.py
+++ expenses/models.py
@@ -1,3 +1,19 @@
+from time import timezone
 from django.db import models
+from authentication.models  import User
 
-# Create your models here.
+
+class Expenses(models.Model):
+    CATEGORIES_OPTIONS=[
+        ('ONLINE_SERVICES','ONLINE_SERVICES'),
+        ('TRAVEL','TRAVEL'),
+        ('FOOD','FOOD'),
+        ('FACTURE','FACTURE'),
+        ('OTHERS','OTHERS')
+    ]
+    
+    category=models.CharField(choices=CATEGORIES_OPTIONS,max_length=50)
+    amount=models.DecimalField(max_digits=10000,decimal_places=2)
+    desciption=models.TextField(max_length=200)
+    owner=models.ForeignKey(to=User,related_name='owner_expense',on_delete=models.CASCADE)
+    creation_date=models.DateTimeField(auto_now_add=True)
